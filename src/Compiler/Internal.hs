2efffc213002da76c9cbc24e8610610a461f9ece
Grant <christiansengrant18@gmail.com>
2025-01-03 21:15:01 -0600

    initial commit for (more) concrete Runic parser transformer type

---
+++ src/Compiler/Internal.hs
@@ -0,0 +1,129 @@
+{-# LANGUAGE Safe #-}
+{-# LANGUAGE InstanceSigs #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Compiler.Internal
+    ( toShuntingYdContext
+    , tokenMapping
+    , RunicKeyword(..)
+    , RunicObject(..)
+    , Token
+    , TokenTracker(..)
+    ) where
+
+import Compiler.Evaluator.Internal as E ( Context, CtxItem(..), SyNum )
+import Compiler.Parser.Internal ( (=+=), TokenEq )
+import Data.Map as M ( fromList, map, Map )
+import Data.Text ( Text )
+
+{-|
+    A type alias for parametrizing what type of numeric value Runic 
+    solves for. 
+-}
+type RunicNum = SyNum
+
+{-|
+    A small alphabet representing the keywords in the Runic language.
+-}
+data RunicKeyword
+    = Keep
+    | On
+    | LBrack
+    | RBrack
+    | Guess
+    | For
+    | Const
+    | Function
+    | Return
+    | If
+    | Then
+    | Else
+    | System
+    | End
+    | NewLine
+    | Expr Text
+    deriving (Eq, Ord, Show)
+
+instance TokenEq RunicKeyword where
+    (=+=) :: RunicKeyword -> RunicKeyword -> Bool
+    (Expr _) =+= (Expr _) = True
+    a =+= b = a == b
+
+{-|
+    A functor for a token that stores the line number it originated
+    from as context.
+-}
+data TokenTracker t = TokenTracker Int t
+    deriving (Eq, Ord)
+
+instance TokenEq t => TokenEq (TokenTracker t) where
+    (=+=) :: TokenTracker t -> TokenTracker t -> Bool
+    (TokenTracker _ tokenA) =+= (TokenTracker _ tokenB)
+        = tokenA =+= tokenB
+
+instance Show t => Show (TokenTracker t) where
+    show :: TokenTracker t -> String
+    show (TokenTracker lineNo x)
+        = show x ++ "(line " ++ show lineNo ++ ")"
+
+instance Functor TokenTracker where
+    fmap :: (a -> b) -> TokenTracker a -> TokenTracker b
+    fmap f (TokenTracker lineNo x)
+        = TokenTracker lineNo (f x)
+
+{-|
+    A value tracked by the global (or local in the case of functions)
+    context of the Runic compiler. Can be a variable with a domain and 
+    initial guess, a constant value, or a pure numerical function.
+-}
+data RunicObject
+    -- | Value, Guess, Min, Max
+    = RnVariable RunicNum RunicNum RunicNum RunicNum
+    -- | Value
+    | RnConst RunicNum
+    -- | # Args, Function
+    | RnFunction Int ([RunicNum] -> RunicNum)
+
+type RunicContext = Map Text RunicObject
+
+{-|
+    Converts the context managed by Runic to the less 
+    information-dense context used by its shunting yard evaluator 
+    implementation.
+-}
+toShuntingYdContext :: RunicContext -> Context
+toShuntingYdContext = M.map toCtxItem
+    where
+        toCtxItem :: RunicObject -> CtxItem
+        toCtxItem (RnVariable value _ _ _) = E.Const value
+        toCtxItem (RnConst value) = E.Const value
+        toCtxItem (RnFunction x y) = E.Function x y
+
+{-|
+    Represents a runic keyword wrapped in a TokenTracker functor
+-}
+type Token = TokenTracker RunicKeyword
+
+{-|
+    Provides a mapping between keywords in valid Runic syntax and 
+    their corresponding Token value.
+-}
+tokenMapping :: Map Text RunicKeyword
+tokenMapping = fromList
+    [ ("keep"   , Keep)
+    , ("on"     , On)
+    , ("["      , LBrack)
+    , ("]"      , RBrack)
+    , ("guess"  , Guess)
+    , ("for"    , For)
+    , ("const"  , Compiler.Internal.Const)
+    , ("fn"     , Compiler.Internal.Function)
+    , ("return" , Return)
+    , ("if"     , If)
+    , ("then"   , Then)
+    , ("else"   , Else)
+    , ("system" , System)
+    , ("end"    , End)
+    ]
+    {-  Expr does not have a corresponding keyword. It corresponds to 
+        expressions that will be parsed for building context for 
+        solving the system later. -}
--- src/Compiler/Parser.hs
+++ src/Compiler/Parser.hs
@@ -1,4 +1,67 @@
 module Compiler.Parser 
-    ( 
+    ( RunicT
     ) where
 
+import Data.Map as M ( insert, lookup, Map )
+import Data.Text ( Text )
+import Data.List ( uncons )
+import Compiler.Internal ( RunicObject, Token, RunicKeyword )
+import Control.Monad.RWS ( asks, get, lift, put, RWST )
+import Control.Monad.Except ( throwError, ExceptT )
+
+{-|
+    A monad transformer built for capturing grammar errors or useful
+    data for computing values to add to the compiler's context.
+
+    This monad transformer type also contains a context that  
+-}
+type RunicT m = RWST
+    (Map Text RunicObject)  -- reader (outer context, empty in the case of the global context)
+    [Text]                  -- writer (captures text from Expr's)
+    (Token, [Token], Map Text RunicObject) -- state (last token processed and tokens left, local context)
+    (ExceptT String m)      -- exception (possible error from grammatically incorrect phrase, i.e. mismatch)
+
+tryPopToken :: Monad m => RunicT m (Maybe Token) 
+tryPopToken = do
+    (prev, inputQueue, localCtx) <- get
+    case uncons inputQueue of
+        Nothing -> return Nothing
+        Just (h, tl) -> do
+            put (prev, tl, localCtx)
+            return (Just h)
+
+grabFromContext :: Monad m => Text -> RunicT m RunicObject
+grabFromContext name = do
+    (_, _, localCtx) <- get
+    -- Try to get the result from the local context first
+    let localResult = M.lookup name localCtx
+    case localResult of
+        Just value -> return value
+        Nothing -> do
+            -- if that fails, then look to the global context in the 
+            -- reader level of the monad stack, returning an error 
+            -- message on failure.
+            globalResult <- asks (M.lookup name)
+            case globalResult of
+                Just value -> return value
+                Nothing -> lift $ throwError 
+                    $ "found undefined value " 
+                    ++ show name 
+                    ++ " in expression"
+
+addToContext :: Monad m => Text -> RunicObject -> RunicT m ()
+addToContext name item = do
+    (prev, inputQueue, localCtx) <- get 
+    let newLocalCtx = insert name item localCtx
+    put (prev, inputQueue, newLocalCtx)        
+
+cycleToken :: Monad m => RunicT m ()
+cycleToken = do
+    token <- tryPopToken
+    (_, inputQueue, localCtx) <- get 
+    case token of
+        Nothing -> return ()
+        Just prevToken -> put (prevToken, inputQueue, localCtx)
+
+runRunicT :: Monad m => RunicT m a -> Either String ()
+runRunicT md =

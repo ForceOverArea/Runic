6af2ceab1e70b37d1b623054aa3ff039a758966a
Grant <christiansengrant18@gmail.com>
2025-01-03 21:15:23 -0600

    slight tweaks to type names in preprocessor

--- src/Compiler/Preprocess.hs
+++ src/Compiler/Preprocess.hs
@@ -5,7 +5,7 @@ module Compiler.Preprocess
     ) where
 
 import Prelude hiding ( lookup, words )
-import Compiler.Parser.Keywords ( tokenMapping, RunicToken(..), Token(..) )
+import Compiler.Internal ( tokenMapping, RunicKeyword(..), TokenTracker(..) )
 import Control.Arrow ( (>>>), (&&&), arr, first )
 import Data.Map ( fromList, keys, lookup, Map )
 import Data.Text ( replace, split, words, Text )
@@ -19,7 +19,8 @@ getLineNumbers lns = zip [1..length lns + 1] lns
     expressible in Runic source code.
 -}
 substSimpleKeywords :: Text -> Text
-substSimpleKeywords = replace "negative" "on [-inf, 0]" 
+substSimpleKeywords 
+    = replace "negative" "on [-inf, 0]" 
     . replace "positive" "on [0, inf]"
 
 {-|
@@ -35,22 +36,22 @@ punctuate txt = foldr puncKw txt (keys tokenMapping)
         puncKw wrd = replace wrd (" " <> wrd <> " ")
 
 -- | Converts a block of text to a token or an expression.
-tokenize :: Text -> Token
+tokenize :: Text -> RunicKeyword
 tokenize t = case lookup t tokenMapping of
     Just tok -> tok
     Nothing -> Expr t
 
--- | Converts a line of text to a list of RunicTokens.
-tokenizeLine :: Int -> Text -> [RunicToken Token]
+-- | Converts a line of text to a list of TokenTrackers.
+tokenizeLine :: Int -> Text -> [TokenTracker RunicKeyword]
 tokenizeLine ln t = 
-    map (RunicToken ln . tokenize) (words $ punctuate t) ++ [RunicToken ln NewLine]
+    map (TokenTracker ln . tokenize) (words $ punctuate t) ++ [TokenTracker ln NewLine]
 
 {-|
     Produces a list of all tokens in the given block of text as well 
     as a map of line numbers to their contained text. In this case 
     the block is the Runic source code.
 -}
-tokenizeSource :: Text -> ([RunicToken Token], Map Int Text)
+tokenizeSource :: Text -> ([TokenTracker RunicKeyword], Map Int Text)
 tokenizeSource = 
     arr substSimpleKeywords
     >>> split (== '\n')
